2af4578ad16df30a5b991e9dea93c89a8fc618c9
Mustafa Eren Çaltı <90278758+mustafacalti@users.noreply.github.com>
2025-09-21 23:55:21 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -275,7 +275,7 @@ app.use((error, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).json({
     error: 'Endpoint bulunamadı',
     availableEndpoints: [
