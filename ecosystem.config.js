aa07f72a0a8e4994062986bc48adf5c98ac6125f
Mustafa Eren Çaltı <90278758+mustafacalti@users.noreply.github.com>
2025-09-22 00:23:35 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'peakfocus-backend',
-      script: 'npm',
-      args: 'start',
-      cwd: '/home/azureuser/pupilica/backend',
+      script: 'server.js',
+      cwd: '/home/erenazure/pupilica/backend',
       env: {
         NODE_ENV: 'production',
         PORT: 5002
@@ -12,24 +11,20 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '512M',
-      error_file: '/var/log/peakfocus-backend-error.log',
-      out_file: '/var/log/peakfocus-backend-out.log'
+      max_memory_restart: '512M'
     },
     {
       name: 'peakfocus-frontend',
-      script: 'npm',
-      args: 'run preview -- --host 0.0.0.0 --port 5173',
-      cwd: '/home/azureuser/pupilica',
+      script: 'npx',
+      args: 'vite preview --host 0.0.0.0 --port 5174',
+      cwd: '/home/erenazure/pupilica',
       env: {
         NODE_ENV: 'production'
       },
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
-      error_file: '/var/log/peakfocus-frontend-error.log',
-      out_file: '/var/log/peakfocus-frontend-out.log'
+      max_memory_restart: '1G'
     }
   ]
 };
